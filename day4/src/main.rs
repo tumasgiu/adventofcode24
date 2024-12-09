551d2b6329b9e328decb75c028feaf8615b726df
tumasgiu <thomas@chocosmos.com>
2024-12-09 19:07:49 +0100

    day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,4 @@
+use crate::Direction::{East, North, NorthEast, NorthWest, South, SouthEast, SouthWest, West};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -7,6 +8,7 @@ fn main() {
     let mut ws = parse("input.txt").unwrap();
     let matches = ws.part1();
     println!("Part 1 : {}", matches);
+    println!("Part 2 : {}", ws.part2());
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -31,27 +33,20 @@ enum Direction {
 impl Direction {
     fn vector(&self) -> (isize, isize) {
         match self {
-            Direction::North => (0, -1),
-            Direction::NorthEast => (1, -1),
-            Direction::East => (1, 0),
-            Direction::SouthEast => (1, 1),
-            Direction::South => (0, 1),
-            Direction::SouthWest => (-1, 1),
-            Direction::West => (-1, 0),
-            Direction::NorthWest => (-1, -1),
+            North => (0, -1),
+            NorthEast => (1, -1),
+            East => (1, 0),
+            SouthEast => (1, 1),
+            South => (0, 1),
+            SouthWest => (-1, 1),
+            West => (-1, 0),
+            NorthWest => (-1, -1),
         }
     }
 
     fn iter() -> impl Iterator<Item = Self> {
         [
-            Direction::North,
-            Direction::South,
-            Direction::East,
-            Direction::West,
-            Direction::NorthEast,
-            Direction::SouthEast,
-            Direction::NorthWest,
-            Direction::SouthWest,
+            North, South, East, West, NorthEast, SouthEast, NorthWest, SouthWest,
         ]
         .into_iter()
     }
@@ -62,6 +57,24 @@ impl WordSearch {
         self.matrix[y][x]
     }
 
+    fn position_from_direction(
+        &self,
+        pos: (usize, usize),
+        direction: &Direction,
+    ) -> Option<(usize, usize)> {
+        let (dx, dy) = direction.vector();
+        let nx = pos.0 as isize + dx;
+        let ny = pos.1 as isize + dy;
+        if nx.is_negative()
+            || ny.is_negative()
+            || ny > self.matrix.len() as isize - 1
+            || nx > self.matrix[ny as usize].len() as isize - 1
+        {
+            return None;
+        }
+        Some((nx as usize, ny as usize))
+    }
+
     fn part1(&mut self) -> usize {
         let mut matches: usize = 0;
         let start_positions = self.chars_map.get(&'X').unwrap();
@@ -69,18 +82,12 @@ impl WordSearch {
             for direction in Direction::iter() {
                 let mut current_char = 'X';
                 let mut current_position = *start_position;
-                let (dx, dy) = direction.vector();
                 for _ in 1..=4 {
-                    let nx = current_position.0 as isize + dx;
-                    let ny = current_position.1 as isize + dy;
-                    if nx.is_negative()
-                        || ny.is_negative()
-                        || ny > self.matrix.len() as isize - 1
-                        || nx > self.matrix[ny as usize].len() as isize - 1
-                    {
+                    let new_pos = self.position_from_direction(current_position, &direction);
+                    if new_pos.is_none() {
                         break;
                     }
-                    let new_pos = (nx as usize, ny as usize);
+                    let new_pos = new_pos.unwrap();
                     let next = self.character_at(new_pos.0, new_pos.1);
                     match current_char {
                         'X' => {
@@ -116,6 +123,55 @@ impl WordSearch {
         }
         matches
     }
+
+    fn part2(self) -> usize {
+        let mut matches: usize = 0;
+        let start_positions: &Vec<(usize, usize)> = self.chars_map.get(&'A').unwrap();
+        for start_position in start_positions {
+            let ne_pos = self.position_from_direction(*start_position, &NorthEast);
+            if ne_pos.is_none() {
+                continue;
+            }
+            let ne_pos = ne_pos.unwrap();
+
+            let sw_pos = self.position_from_direction(*start_position, &SouthWest);
+            if sw_pos.is_none() {
+                continue;
+            }
+            let sw_pos = sw_pos.unwrap();
+
+            let nw_pos = self.position_from_direction(*start_position, &NorthWest);
+            if nw_pos.is_none() {
+                continue;
+            }
+            let nw_pos = nw_pos.unwrap();
+
+            let se_pos = self.position_from_direction(*start_position, &SouthEast);
+            if se_pos.is_none() {
+                continue;
+            }
+            let se_pos = se_pos.unwrap();
+
+            if !((self.character_at(ne_pos.0, ne_pos.1) == 'M'
+                && self.character_at(sw_pos.0, sw_pos.1) == 'S')
+                || (self.character_at(ne_pos.0, ne_pos.1) == 'S'
+                    && self.character_at(sw_pos.0, sw_pos.1) == 'M'))
+            {
+                continue;
+            }
+
+            if !((self.character_at(nw_pos.0, nw_pos.1) == 'M'
+                && self.character_at(se_pos.0, se_pos.1) == 'S')
+                || (self.character_at(nw_pos.0, nw_pos.1) == 'S'
+                    && self.character_at(se_pos.0, se_pos.1) == 'M'))
+            {
+                continue;
+            }
+
+            matches += 1;
+        }
+        matches
+    }
 }
 
 fn parse(filename: &str) -> Result<WordSearch, Box<dyn std::error::Error>> {
@@ -147,6 +203,6 @@ fn parse(filename: &str) -> Result<WordSearch, Box<dyn std::error::Error>> {
 #[test]
 fn test_day4() {
     let mut ws = parse("test-input.txt").unwrap();
-    let matches = ws.part1();
-    assert_eq!(matches, 18);
+    assert_eq!(ws.part1(), 18);
+    assert_eq!(ws.part2(), 9)
 }
